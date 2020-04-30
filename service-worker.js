802cdcbfd50dc5bfb267b64be546777e2188ed26
Israel Dzib <a16016344@alumnos.uady.mx>
2020-04-29 20:12:35 -0500

    Correción de cache

--- service-worker.js
+++ service-worker.js
@@ -775,7 +775,7 @@ self.addEventListener('activate', function(e) {
   e.waitUntil(
     caches.keys().then(function(keyList) {
       return Promise.all(keyList.map(function(key) {
-        if (key !== himnosCache && key !== cacheName)) {
+        if (key !== himnosCache && key !== cacheName) {
           console.log('[ServiceWorker] Removing old cache', key);
           return caches.delete(key);
         }
