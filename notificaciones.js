4ebb6b7faba1e42c4763f992e2532933438c9b28
Israel Dzib <a16016344@alumnos.uady.mx>
2020-04-30 21:38:48 -0500

    Se añade botón para ir al blog

--- notificaciones.js
+++ notificaciones.js
@@ -1,4 +1,5 @@
 var actualizacion = "Mayo 2020"
+var blogUrl = "http://www.himnariop.com/2020/04/30/actualizacion-mayo-2020/"
 var Update = localStorage.getItem("okUpdate");
 var filesToCache = [
 	//UI
@@ -34,12 +35,12 @@ var filesToCache = [
 if (version == actualizacion){
     if (actualizacion != Update){
         document.write(`<div style="border-style:dotted;display:block;"><p>Tu aplicación está actualizada.</p>`); 
-        document.write(`<p><button onclick="okUpdate();">OK</button></p></div>`); 
+        document.write(`<p><button onclick="okUpdate();">Aceptar</button> <button onclick="href(blogUrl);">Más información</button></p></div>`); 
     }
 } else {
     document.write(`<div style="border-style:dotted;display:block;"><p>Versión actual: ${version}</p>`); 
     document.write(`<p>Última versión: ${actualizacion}</p>`);
-    document.write(`<p><button id="boton_actualizar" onclick="actualizarApp();">Actualizar ahora</button></p></div>`);  
+    document.write(`<p><button id="boton_actualizar" onclick="actualizarApp();">Actualizar ahora</button> <button onclick="href(blogUrl);">Más información</button></p></div>`);  
 }
 
 function actualizarApp(){
@@ -56,4 +57,8 @@ function actualizarApp(){
 function okUpdate(){
     localStorage.setItem("okUpdate",actualizacion);
     location.reload();
+}
+
+function href(enlace){
+    window.open(enlace);
 }
