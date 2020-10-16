deb4fb4dd4a82d5beb989abc4ce7187042d333f5
suzu <suzu.nkdca@gmail.com>
2020-10-15 21:01:04 -0500

    Version 2

---
+++ src/app/components/preguntas/preguntas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-preguntas',
+  templateUrl: './preguntas.component.html',
+  styleUrls: ['./preguntas.component.scss']
+})
+export class PreguntasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
